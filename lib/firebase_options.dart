39da9a7081200980d69e2e82b73b47967beaa828
Dilsha Thathsarani <86911572+Dilsha-thathsarani@users.noreply.github.com>
2025-03-05 12:39:18 +0530

    Add Firebase integration to app

---
+++ lib/firebase_options.dart
@@ -0,0 +1,16 @@
+// lib/firebase_options.dart
+
+import 'package:firebase_core/firebase_core.dart';
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    return FirebaseOptions(
+      apiKey: "AIzaSyBK8USMNY9xYqXdb0c3CKT2a4Nro3z7qEk",
+      appId: "1:736165275645:android:0d6af5015b594083211b94",
+      messagingSenderId: "736165275645",
+      projectId: "homzes-76cc1",
+      authDomain: "homzes-76cc1.firebaseapp.com",
+      storageBucket: "homzes-76cc1.firebasestorage.app",
+    );
+  }
+}
